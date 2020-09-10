f88f4edc5c45b68d5239898e10a0321774fed202
System-in-a-System <textconstruction@potonmail.com>
2020-09-10 16:46:27 +0100

    Improve code structure & robustness

--- SwedishID-Decryptor/Program.cs
+++ SwedishID-Decryptor/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using static System.Console;
 using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace SwedishID_Decryptor
 {
@@ -34,126 +35,27 @@ namespace SwedishID_Decryptor
                 Write("Last name: ");
                 lastName = ReadLine();
 
-                Write("Social Security Number: ");
-                socialSecurityNumber = ReadLine();
+                Write("Social Security Number (YYYYMMDD-XXXX): ");
+                socialSecurityNumber = ValidateSSNNumber(ReadLine());
             }
+
+
+
+
+            // Decrypt information from the input Social Security Number: 
             
-     
+            // Define gender 
+            string gender = DefineGender(socialSecurityNumber);
 
-            //__Check user input for validity_________________________________________________________________________//
-              
-            // Purify the input number-string 
-            string socialSecurityNumberClean = socialSecurityNumber.Replace("-", "");
-            
-            // If the purified number-string is NOT convertable to float = user input is NOT valid
-            if(!float.TryParse(socialSecurityNumberClean, out float socialSecurityNumberIntegerified))
-            {
-               // terminate the program softly
-                WriteLine("Invalid Social Security Number");
-                ReadLine();
-                return;
-            }
-            //_____________________________________________________________________________________________________//
+            // Define age 
+            int age = DefineAge(socialSecurityNumber);
 
-
-
-
-            // Information to decrypt from the input data: 
-            string gender = "Unknown";
-            int age = 0;
-            string generation = "Unknown";
-            string generationInformation = "Undefined";
-
-
-
-            // --------------------------------------------- Define gender --------------------------------------------------//
-            int genderNumber = int.Parse(socialSecurityNumber.Substring(socialSecurityNumber.Length - 2, 1));
-            bool isFemale = genderNumber % 2 == 0;
-            gender = isFemale ? "Female" : "Male";
-
-
-
-            // ---------------------------------------------- Define age ----------------------------------------------------//
-            // Assign the default value to birthDate
-            DateTime birthDate = DateTime.Now;
-
-
-            // Retrieve birthday date from SSN:
-
-            // If we are dealing with the short version of SSN...
-            if (socialSecurityNumber.Length >= 10 && socialSecurityNumber.Length <= 11)
-            {
-                birthDate = DateTime.ParseExact(socialSecurityNumber.Substring(0, 6), "yyMMdd", CultureInfo.InvariantCulture);
-            }
-            // If we are dealing with the long version of SSN...
-            else if (socialSecurityNumber.Length >= 12 && socialSecurityNumber.Length <= 13)
-            {
-                birthDate = DateTime.ParseExact(socialSecurityNumber.Substring(0, 8), "yyyyMMdd", CultureInfo.InvariantCulture);
-            }
-            // if the length of SSN is not valid...
-            else
-            {
-                // terminate the program softly
-                WriteLine("Invalid Social Security Number...");
-                ReadLine();
-                return;
-            }
-
-
-            // Calculate age based on retrieved data
-            age = DateTime.Now.Year - birthDate.Year;
-
-            // Possible age correction depending on the day of the year
-            if ((birthDate.Month > DateTime.Now.Month) || (birthDate.Month == DateTime.Now.Month && birthDate.Day > DateTime.Now.Day))
-            {
-                age--;
-            }
-
-
-
-            // ------------------------------------------ Define generation ------------------------------------------------- // 
-            // source: http://socialmarketing.org/archives/generations-xy-z-and-the-others/
-
-            int birthYear = birthDate.Year;
-
-            if (birthYear >= 1995)
-            {
-                generation = "Generation Z";
-                generationInformation = "smartphones, social media, never knowing a country not at war.";
-            }
-            else if (birthYear >= 1977)
-            {
-                generation = "Millennials";
-                generationInformation = "the Great Recession, the technological explosion of the internet and social media, 9/11.";
-            }
-            else if (birthYear >= 1965)
-            {
-                generation = "Lost generation";
-                generationInformation = "the end of the Cold war, the lowest voting participation rate, skepticism.";
-            }
-            else if (birthYear >= 1946)
-            {
-                generation = "Baby-Boomers generation";
-                generationInformation = "post-WWII optimism, the cold war, and the hippie movement.";
-            }
-            else if (birthYear >= 1928)
-            {
-                generation = "Post-War generation";
-                generationInformation = "post-war economic boom, Cold War tensions, the potential for nuclear war.";
-            }
-            else if (birthYear >= 1922)
-            {
-                generation = "War generation";
-                generationInformation = "the Korean War, the Second World War, the Cold War.";
-            }
-            else if (birthYear >= 1912)
-            {
-                generation = "Depression generation";
-                generationInformation = "the Great Depression, the Global Unrest.";
-            }     
+            // Define generation
+            string generation = DefineGeneration(socialSecurityNumber);
             
 
-
+            
+            
             // Clear the screen
             Clear();
             Beep(200, 500);
@@ -164,10 +66,123 @@ namespace SwedishID_Decryptor
                       $"\nSocial Security Number: {socialSecurityNumber} " +
                       $"\nGender: {gender}" +
                       $"\nAge: {age}" +
-                      $"\n\nGeneration: {generation}" +
-                      $"\nShaping events: {generationInformation}");
+                      $"\n\nGeneration: {generation}");
 
             ReadLine();
+        }
+
+
+
+
+        // Method that checks user input for validity & formats it according to uniformed 12-digit format
+        static string ValidateSSNNumber(string inputNumber)
+        {
+            // Initialize SSN patterns in two versions
+            string pattern1 = @"^\d{6}-\d{4}$";
+            string pattern2 = @"^\d{8}-\d{4}$";
+
+            Regex regex1 = new Regex(pattern1);
+            Regex regex2 = new Regex(pattern2);
+
+
+            // Check inputNumber for validity
+            bool validSocialSecurityNumber = regex1.IsMatch(inputNumber) || regex2.IsMatch(inputNumber);
+
+
+
+            // Set up the loop that will prompt the user to enter SSN until it is valid
+            while (!validSocialSecurityNumber)
+            {
+                WriteLine("Invalid Social Security Number...");
+                Write("Please, try again: ");
+                inputNumber = ReadLine();
+                validSocialSecurityNumber = regex1.IsMatch(inputNumber) || regex2.IsMatch(inputNumber);
+            }
+
+            // Unified SSN format: YYYYMMDD-XXXX
+            string unifiedSocialSecurityNumber;
+
+            // If the short pattern of SSN was followed...
+            if (regex1.IsMatch(inputNumber))
+            {
+                // complement the pattern 
+                int shortYearVersion = int.Parse(inputNumber.Substring(0, 2));
+
+                if (shortYearVersion > 20 && shortYearVersion <= 99)
+                {
+                    unifiedSocialSecurityNumber = "19" + inputNumber;
+                }
+                else
+                {
+                    unifiedSocialSecurityNumber = "20" + inputNumber;
+                }
+
+            }
+            // If the long pattern of SSN was followed...
+            else
+            {
+                // assign the long pattern as unifiedSocialSecurityNumber
+                unifiedSocialSecurityNumber = inputNumber;
+            }
+
+
+            return unifiedSocialSecurityNumber;
+        }
+
+
+        // Method that retrieves gender information from Social Security Number
+        static string DefineGender(string inputSocialSecurityNumber)
+        {
+            string gender;
+            int genderNumber = int.Parse(inputSocialSecurityNumber.Substring(inputSocialSecurityNumber.Length - 2, 1));
+            bool isFemale = genderNumber % 2 == 0;
+            gender = isFemale ? "Female" : "Male";
+            return gender;
+        }
+
+
+        // Method that retrieves age information from Social Security Number
+        static int DefineAge(string inputSocialSecurityNumber)
+        {
+            // Retrieve the birth date from input social security number
+            DateTime birthDate = DateTime.ParseExact(inputSocialSecurityNumber.Substring(0, 8), "yyyyMMdd", CultureInfo.InvariantCulture);
+            
+            // Calculate age based on retrieved data
+            int age = DateTime.Now.Year - birthDate.Year;
+
+            // Possible age correction depending on the day of the year
+            if ((birthDate.Month > DateTime.Now.Month) || (birthDate.Month == DateTime.Now.Month && birthDate.Day > DateTime.Now.Day))
+            {
+                age--;
+            }
+
+            return age;
+        }
+
+
+        // Method that retrieves generation information from Social Security Number
+        static string DefineGeneration(string inputSocialSecurityNumber)
+        {
+            // Retrieve the birth date from input social security number
+            DateTime birthDate = DateTime.ParseExact(inputSocialSecurityNumber.Substring(0, 8), "yyyyMMdd", CultureInfo.InvariantCulture);
+
+            // Retrieve the birth year
+            int birthYear = birthDate.Year;
+
+            // Declare a target variable
+            string generation;
+
+            // Define the target variable
+            if (birthYear >= 1995) { generation = "Generation Z"; }
+            else if (birthYear >= 1977) { generation = "Millennials"; }
+            else if (birthYear >= 1965) { generation = "Lost generation"; }
+            else if (birthYear >= 1946) { generation = "Baby-Boomers generation"; }
+            else if (birthYear >= 1928) { generation = "Post-War generation"; }
+            else if (birthYear >= 1922) { generation = "War generation"; }
+            else if (birthYear >= 1912) { generation = "Depression generation"; }
+            else { generation = "Undefined"; }
+
+            return generation;
         }
     }
 }
